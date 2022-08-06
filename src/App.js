a972cae45222e45b3aedb44fbe1ef62fc7ff7e49
Princewill Iroka <princewilliroka@outlook.com>
2022-08-06 02:07:18 +0100

    Setup router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Chat from "./components/Chat";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/chat" element={<Chat />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
