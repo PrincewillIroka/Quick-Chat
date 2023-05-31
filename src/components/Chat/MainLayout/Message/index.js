97d7cea05f5f5f1546147b2f5ebaf64e79a4728e
Princewill Iroka <princewilliroka@outlook.com>
2023-06-01 00:31:43 +0100

    Updated Message component

--- src/components/Chat/MainLayout/Message/index.js
+++ src/components/Chat/MainLayout/Message/index.js
@@ -1,5 +1,28 @@
 import React from "react";
+import "./Message.css";
 
 export default function Message({ message }) {
-  return <div>{message.message}</div>;
+  const isSameSender = (message) => {
+    return message.sender._id === "631535b15cd9aab7ef9a37a3"; //Todo: remove this
+  };
+
+  return (
+    <div
+      className={`message-container ${
+        isSameSender(message) && "message-container-align-right"
+      }`}
+    >
+      <img
+        className="message-sender-photo"
+        src="https://media.istockphoto.com/photos/pleasant-young-indian-woman-freelancer-consult-client-via-video-call-picture-id1300972573?b=1&k=20&m=1300972573&s=170667a&w=0&h=xuAsEkMkoBbc5Nh-nButyq3DU297V_tnak-60VarrR0="
+      />
+      <div className="message-details">
+        <div className="message-info">
+          <span className="message-owner">{message.sender.name}</span>
+          <span className="message-time">10:05 AM</span>
+        </div>
+        <span className="message-original">{message.message}</span>
+      </div>
+    </div>
+  );
 }
