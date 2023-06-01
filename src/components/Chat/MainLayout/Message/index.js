4d1beff6c53cfce547cbf59ee29e9f01c320c09c
Princewill Iroka <princewilliroka@outlook.com>
2023-06-01 23:33:42 +0100

    Update isSameSender

--- src/components/Chat/MainLayout/Message/index.js
+++ src/components/Chat/MainLayout/Message/index.js
@@ -5,18 +5,18 @@ import { generateInitials } from "../../../../utils";
 export default function Message({ message }) {
   const { sender = {}, content = "" } = message;
 
-  const isSameSender = (message) => {
+  const isSameSender = (sender) => {
     return sender?._id === "631535b15cd9aab7ef9a37a3"; //Todo: remove this
   };
 
   return (
     <div
       className={`message-container ${
-        isSameSender(message) && "message-container-align-right"
+        isSameSender(sender) && "message-container-align-right"
       }`}
     >
       {sender?.photo ? (
-        <img className="message-sender-photo" src={sender?.photo} />
+        <img className="message-sender-photo" src={sender?.photo} alt="Sender"/>
       ) : (
         <span className="message-sender-photo message-sender-initial">
           {generateInitials(sender?.name)}
