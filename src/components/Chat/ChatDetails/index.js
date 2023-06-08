6e1fa88120273ed9ba4e745064ea20f303975a4c
Princewill Iroka <princewilliroka@outlook.com>
2023-06-08 16:17:36 +0100

    Update ChatDetails

--- src/components/Chat/ChatDetails/index.js
+++ src/components/Chat/ChatDetails/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import "./ChatDetails.css";
 
 export default function ChatDetails() {
-  return <div>ChatDetails</div>;
+  return (
+    <div className="chat-details-container">
+      <div className="chat-details-header">Chat Details</div>
+      <div className="chat-details-body"></div>
+    </div>
+  );
 }
