3d98ce1f7cc47e37ebe6defb7cc2c964f036be65
Princewill Iroka <princewilliroka@outlook.com>
2023-07-14 22:39:29 +0100

    Added new message audio alert

--- src/sockets/chatSockets.js
+++ src/sockets/chatSockets.js
@@ -1,10 +1,13 @@
-import NotificationTone from "constants/mixkit-correct-answer-tone-2870.wav";
+import NotificationTone from "static/media/mixkit-bubble-pop-up-alert-notification-2357.wav";
+
+const audio = new Audio(NotificationTone);
 
 const chatSockets = (socket, state, dispatch) => {
   socket.on("new-message-received", (payload) => {
     if (payload) {
-      const audio = new Audio(NotificationTone);
-      audio.play();
+      if (audio.pause) {
+        audio.play().catch(console.warn);
+      }
       dispatch({ type: "UPDATE_CHAT", payload });
     }
   });
