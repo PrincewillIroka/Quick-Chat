f924afb0e4cc43e9f2a46a06b68bdb310759a401
Princewill Iroka <princewilliroka@outlook.com>
2024-02-17 12:37:51 +0100

    Add ChatNotifications tab

--- src/components/Chat/ChatDetails/index.js
+++ src/components/Chat/ChatDetails/index.js
@@ -4,6 +4,7 @@ import { BsPeople, BsInfoCircle } from "react-icons/bs";
 import "./ChatDetails.css";
 import ChatMedia from "./ChatMedia";
 import ChatParticipants from "./ChatParticipants";
+import ChatNotifications from "./ChatNotifications";
 
 export default function ChatDetails({ colorSchema }) {
   const [activeDetail, setActiveDetail] = useState("ChatMedia");
@@ -51,26 +52,32 @@ export default function ChatDetails({ colorSchema }) {
           </div>
           <div
             className={`chat-details-icon-container ${
-              getActiveDetail("notification") &&
+              getActiveDetail("ChatNotifications") &&
               "chat-details-icon-active-container"
             }`}
-            onClick={() => setActiveDetail("notification")}
+            onClick={() => setActiveDetail("ChatNotifications")}
           >
             <IoNotificationsOutline
               className={`chat-detail-icon ${
-                getActiveDetail("notification") && "chat-detail-active-icon"
+                getActiveDetail("ChatNotifications") &&
+                "chat-detail-active-icon"
               }`}
             />
           </div>
         </div>
         {getActiveDetail("ChatParticipants") && (
-          <div className="chat-media-wrapper">
+          <div className="chat-detai-single-wrapper">
             <ChatParticipants />
           </div>
         )}
         {getActiveDetail("ChatMedia") && (
-          <div className="chat-media-wrapper">
+          <div className="chat-detai-single-wrapper">
             <ChatMedia />
+          </div>
+        )}
+        {getActiveDetail("ChatNotifications") && (
+          <div className="chat-detai-single-wrapper">
+            <ChatNotifications />
           </div>
         )}
       </div>
