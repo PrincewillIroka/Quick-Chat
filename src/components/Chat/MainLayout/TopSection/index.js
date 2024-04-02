658bf7d5975d819f91d9f0f84b830f5071946794
Princewill Iroka <princewilliroka@outlook.com>
2024-04-02 17:51:12 +0100

    Chore: Update object declaration

--- src/components/Chat/MainLayout/TopSection/index.js
+++ src/components/Chat/MainLayout/TopSection/index.js
@@ -180,7 +180,7 @@ function TopSection({ selectedChat }) {
         <div className="user-info-col-1">
           {/* <span className="user-info-name">{chat_name}</span> */}
           <div className="user-info-photo-or-initial-wrapper">
-            {participants.slice(0, 3).map((participant, index) => {
+            {participants.slice(0, 3).map((participant = {}, index) => {
               const checkSameSender = isSameSender(participant, user);
               participant = checkSameSender ? user : participant;
               const { name = "", photo = "" } = participant;
